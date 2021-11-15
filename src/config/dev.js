c1d68e7d3b25e3290b1e19988de11fc1509c3a8c
mubashir <mubashir11131719@gmail.com>
2021-11-15 15:46:18 +0530

    uri fixed

--- src/config/dev.js
+++ src/config/dev.js
@@ -2,5 +2,6 @@ export const config = {
   secrets: {
     jwt: 'learneverything'
   },
-  dbUrl: 'mongodb://localhost:27017/api-design'
+  dbUrl:
+    'mongodb+srv://mubashir:password911@myapi.aod9n.mongodb.net/db?retryWrites=true&w=majority'
 }
